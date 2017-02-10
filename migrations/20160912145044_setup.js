ce7507d682afe96a625b9c9ee022e2f070b55618
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-02-10 13:05:07 -0500

    Add is_admin property to user migration.

--- migrations/20160912145044_setup.js
+++ migrations/20160912145044_setup.js
@@ -4,23 +4,28 @@ exports.up = knex =>
     table.increments('id');
     table.string('username').unique().notNullable();
     table.string('name');
+    table.boolean('is_admin');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.index('username');
-  }).createTable('password', (table) => {
+  })
+  .createTable('password', (table) => {
     table.increments('id');
     table.integer('user_id').references('user.id').notNullable().onDelete('CASCADE');
     table.string('password_hash').notNullable();
     table.timestamp('updated_at').defaultTo(knex.fn.now());
     table.boolean('valid').defaultsTo(true);
-  }).createTable('email', (table) => {
+  })
+  .createTable('email', (table) => {
     table.increments('id');
     table.integer('user_id').references('user.id').notNullable().onDelete('CASCADE');
     table.string('address').unique().notNullable();
     table.boolean('verified').defaultsTo(false);
-  }).createTable('role', (table) => {
+  })
+  .createTable('role', (table) => {
     table.increments('id');
     table.string('name').unique().notNullable();
-  }).createTable('user_role', (table) => {
+  })
+  .createTable('user_role', (table) => {
     table.integer('user_id').references('user.id').notNullable();
     table.integer('role_id').references('role.id').notNullable();
   });
