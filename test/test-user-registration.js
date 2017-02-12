94964a996056d7d19e965cd8d579c04c040455dc
Shayde Nofziger <sln7755@rit.edu>
2017-02-12 13:39:43 -0500

    Unit test for successful User registration

---
+++ test/test-user-registration.js
@@ -0,0 +1,26 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+/* eslint-disable */
+const should = chai.should();
+/* eslint-enable */
+
+chai.use(chaiHttp);
+
+describe('register test_user_registration success', () => {
+  it('should succeed register test_user_registration on /api/user POST', (done) => {
+    chai.request(server)
+      .post('/api/user')
+      .send({
+        username: 'test_user_registration',
+        password: 'password_registration',
+        email: 'test_registration@test.com',
+        name: 'Test User Registration',
+      })
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
+});
