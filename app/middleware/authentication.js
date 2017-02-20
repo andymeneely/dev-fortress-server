6c4ceb8cd2a70a847533dd2d3e47e8f6cf2c9aaa
Shayde Nofziger <sln7755@rit.edu>
2017-02-20 12:22:15 -0500

    Verify using the user model attached to req.

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -1,4 +1,5 @@
 const auth = require('../lib/authentication');
+const User = require('../models/user');
 
 /**
  * Error handler for verifyToken errors
@@ -17,7 +18,7 @@ function tokenErrorHandler(err, res) {
       message: 'jwt must be provided',
     });
   }
-  console.log(err);
+  console.error(err);
   return res.status(401).json({
     error: 'Unauthorized',
     message: 'You are not authenticated.',
@@ -48,37 +49,75 @@ function validateAuthentication(req, res, next) {
 
   return auth.verifyToken(authMatch[1], (err) => {
     if (err) {
-      tokenErrorHandler(err, res);
+      return tokenErrorHandler(err, res);
     }
 
     return next();
   });
 }
 
-/**
- * Verify the user token, and ensure that the user is an Administrator.
- */
-function verifyAdministrator(req, res, next) {
+function validateAuthenticationAttachUser(req, res, next) {
   const authHeader = req.get('Authorization');
+  if (!authHeader) {
+    return res.status(401).send({
+      error: 'Unauthorized',
+      message: 'You are not authenticated.',
+    });
+  }
+
   const authMatch = authHeader.match(/Bearer (.*)$/);
 
-  // Decoded is the decoded JWT token returned upon successful verification
+  if (!authMatch) {
+    return res.status(401).send({
+      error: 'Unauthorized',
+      message: 'You are not authenticated.',
+    });
+  }
+
   return auth.verifyToken(authMatch[1], (err, decoded) => {
     if (err) {
-      tokenErrorHandler(err, res);
+      return tokenErrorHandler(err, res);
     }
-    if (!(decoded.is_admin)) {
-      return res.status(403).json({
-        error: 'Forbidden',
-        message: 'You must be an Administrator',
+    const userId = decoded.userId;
+    return User.forge({ id: userId })
+      .fetch({ withRelated: 'roles' })
+      .then((user) => {
+        req.user = user.serialize();
+        return next();
+      })
+      .catch((fetchErr) => {
+        console.error(fetchErr);
+        res.status(500).json({
+          error: 'Unknown Error',
+        });
       });
-    }
-    return next();
   });
+}
+
+/**
+ * Pre-req: validateAuthenticationAttachUser
+ * Ensure that the user is an Administrator.
+ */
+function verifyAdministrator(req, res, next) {
+  if (!req.user) {
+    console.error('validateAuthenticationAttachUser middleware is a pre-requisite of verifyAdministrator middleware.');
+    return res.status(500).json({
+      error: 'Internal Server Error',
+      message: 'The server is configured incorrectly.',
+    });
+  }
+  if (!req.user.is_admin) {
+    return res.status(403).json({
+      error: 'Forbidden',
+      message: 'You must be an Administrator',
+    });
+  }
+  return next();
 }
 
 
 module.exports = {
   validateAuthentication,
+  validateAuthenticationAttachUser,
   verifyAdministrator,
 };
