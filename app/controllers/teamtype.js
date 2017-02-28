52a80a475d4e6e0cdfa4acf1f358ff7f7b13681f
Shayde Nofziger <sln7755@rit.edu>
2017-02-28 12:27:10 -0500

    Get TeamType by id; type coercion

--- app/controllers/teamtype.js
+++ app/controllers/teamtype.js
@@ -4,17 +4,70 @@
 const TeamType = require('../models/teamtype');
 
 /**
+ * Internal helper function. Serializes and performs type coercion
+ * Returns the serialized and coerced collection.
+ */
+function serializeAndCoerce(teamTypeCollection) {
+  const teamTypeCollectionJson = teamTypeCollection.serialize();
+  // Type coercion for boolean values
+  teamTypeCollectionJson.forEach((teamType) => {
+    teamType.initial_mature = !!teamType.initial_mature;
+    teamType.disabled = !!teamType.disabled;
+  });
+  return teamTypeCollectionJson;
+}
+
+/**
  * Get all TeamTypes in the database
  * @param  {Express.Request}   req  - the request object
  * @param  {Express.Response}  res  - the response object
- * @param  {Function} next - pass to next route handler
  */
 function getTeamTypes(req, res) {
   return TeamType.fetchAll()
-  .then(collection => res.json(collection.serialize()))
+  .then((collection) => {
+    const teamTypes = serializeAndCoerce(collection);
+    res.json(teamTypes);
+  })
   .catch((err) => {
+    // 404
+    if (err.message === 'EmptyResponse') {
+      return res.status(404)
+      .json({
+        error: 'NotFound',
+      });
+    }
     console.error(err);
-    res.status(500).json({
+    return res.status(500).json({
+      error: 'UnknownError',
+    });
+  });
+}
+
+/**
+ * Get a TeamType by id parameter
+ * @param  {Express.Request}   req  - the request object
+ * @param  {Express.Response}  res  - the response object
+ */
+function getTeamTypeById(req, res) {
+  return TeamType.where('id', req.params.id).fetch({
+    require: true,
+  })
+  .then((result) => {
+    const teamType = serializeAndCoerce(result);
+    res.json(teamType);
+  })
+  .catch((err) => {
+    // 404
+    if (err.message === 'EmptyResponse') {
+      return res.status(404)
+      .json({
+        error: 'NotFound',
+      });
+    }
+    // Unknown error
+    console.error(err);
+    return res.status(500)
+    .json({
       error: 'UnknownError',
     });
   });
@@ -24,14 +77,13 @@ function getTeamTypes(req, res) {
  * Create a new TeamType
  * @param  {Express.request}   req  - the request object
  * @param  {Express.Response}   res  - the response object
- * @param  {Function} next - pass to next route handler
  */
 function createTeamType(req, res) {
   return TeamType.forge(req.body)
   .save().then(teamtype => res.json(teamtype.serialize()))
   .catch((err) => {
     console.error(err);
-    res.status(500).json({
+    return res.status(500).json({
       error: 'UnknownError',
     });
   });
@@ -40,5 +92,6 @@ function createTeamType(req, res) {
 
 module.exports = {
   getTeamTypes,
+  getTeamTypeById,
   createTeamType,
 };
