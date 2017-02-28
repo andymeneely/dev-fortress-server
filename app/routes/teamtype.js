5a3bcc5e16267e22b7d0032bd384e6efe83c430f
Shayde Nofziger <sln7755@rit.edu>
2017-02-28 12:29:33 -0500

    verifyProf middleware for post route; GET by ID

--- app/routes/teamtype.js
+++ app/routes/teamtype.js
@@ -1,9 +1,16 @@
 const teamtypeController = require('../controllers/teamtype');
+const authenticationMiddleware = require('../middleware/authentication');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/', teamtypeController.getTeamTypes);
-router.post('/', teamtypeController.createTeamType);
+router.get('/:id', teamtypeController.getTeamTypeById);
+router.post(
+  '/',
+  authenticationMiddleware.validateAuthenticationAttachUser,
+  authenticationMiddleware.verifyProfessor,
+  teamtypeController.createTeamType
+);
 
 module.exports = router;
