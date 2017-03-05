f0f0b345f159951876fd44321c91eccfbb1998a8
Shayde Nofziger <sln7755@rit.edu>
2017-03-05 14:38:31 -0500

    Migration for Rumor table

--- migrations/20160912145044_setup.js
+++ migrations/20160912145044_setup.js
@@ -37,10 +37,18 @@ exports.up = knex =>
     table.integer('initial_resources').notNullable();
     table.integer('initial_mindset').notNullable();
     table.boolean('disabled').defaultsTo(false);
+  })
+  .createTable('rumor', (table) => {
+    table.increments('id');
+    table.string('name').unique().notNullable();
+    table.string('description').notNullable();
+    table.integer('event_id');
+    table.boolean('disabled').defaultTo(false);
   });
 
 exports.down = (knex, Promise) =>
   Promise.all([
+    knex.schema.dropTable('rumor'),
     knex.schema.dropTable('teamtype'),
     knex.schema.dropTable('password'),
     knex.schema.dropTable('email'),
