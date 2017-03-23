a6cfe22918b494dda7311e33174456cbdf49a217
Shayde Nofziger <sln7755@rit.edu>
2017-03-22 22:45:32 -0400

    Use TIMEOUT constant for all tests.

--- test/data/constants.js
+++ test/data/constants.js
@@ -1,7 +1,11 @@
 const API_LOGIN_URL = '/api/user/login';
 const API_USER_REGISTER_URL = '/api/user';
 
+// Milliseconds – How long each test should wait before checking response values.
+const TIMEOUT = 10;
+
 module.exports = {
   API_LOGIN_URL,
   API_USER_REGISTER_URL,
+  TIMEOUT,
 };
