1814068a37c027d4ffd7df34eeb0c1a5505028f0
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-04 13:24:41 -0400

    API tests for modifying an existing Team

--- test/tests/api/test-team-update.js
+++ test/tests/api/test-team-update.js
@@ -1,14 +1,59 @@
 const API_TEAM_UPDATE_URL = require('../../data/constants').API_TEAM_UPDATE_URL;
+const API_TEAM_LOGIN_URL = require('../../data/constants').API_TEAM_LOGIN_URL;
 const knex = require('../../../app/lib/db');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const server = require('../../../app');
 
 const should = chai.should();
+const assert = chai.assert;
 
 chai.use(chaiHttp);
 
+let teamToken = null;
+const teamIdToUpdate = 1;
+
+/**
+ * Sends PATCH request with the given requestBody. Passes error/response to callback
+ * @param {Object} requestBody - Object containing fields to be updated and their values
+ * @param {*} callback - Callback function once request returns
+ */
+function sendPatchTeamRequest(requestBody, callback) {
+  chai.request(server)
+    .patch(`${API_TEAM_UPDATE_URL}${teamIdToUpdate}`)
+    .set('Authorization', `Bearer ${teamToken}`)
+    .send(requestBody)
+    .end(callback);
+}
+
 describe('Team Update API Tests', () => {
+  before((done) => {
+    // Authenticate a Team to perform protected queries.
+    function authenticateTeam() {
+      const reqURL = `${API_TEAM_LOGIN_URL}`;
+      chai.request(server)
+        .post(reqURL)
+        .send({
+          link: '1234567',
+        })
+        .end((err, res) => {
+          if (res) {
+            teamToken = res.body.token;
+            done();
+          }
+        });
+    }
+
+    // Run initial migrations and seed db
+    knex.migrate.rollback()
+      .then(() => {
+        knex.migrate.latest()
+          .then(() => {
+            knex.seed.run()
+              .then(() => authenticateTeam());
+          });
+      });
+  });
   beforeEach((done) => {
     // Run initial migrations and seed db
     knex.migrate.rollback()
@@ -23,23 +68,76 @@ describe('Team Update API Tests', () => {
 
   describe('Success', () => {
     it('An existing Team can update their name field', (done) => {
-      const newName = 'Slow Squadron';
-      const idToUpdate = 1;
-      const reqURL = `${API_TEAM_UPDATE_URL}${idToUpdate}`;
-      chai.request(server)
-        .patch(reqURL)
-        .send({ name: newName })
-        .end((err, res) => {
-          res.statusCode.should.equal(200);
-          res.body.id.should.equal(idToUpdate);
-          res.body.name.should.equal(newName);
-          should.not.exist(err);
-          done();
-        });
+      const requestBody = { name: 'Slow Squadron' };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(200);
+        res.body.id.should.equal(teamIdToUpdate);
+        res.body.name.should.equal(requestBody.name);
+        should.not.exist(err);
+        done();
+      });
     });
   });
 
   describe('Fail', () => {
+    it('An existing Team cannot update their game_id field', (done) => {
+      const requestBody = { game_id: 1 };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
 
+    it('An existing Team cannot update their type_id field', (done) => {
+      const requestBody = { type_id: 1 };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('An existing Team cannot update their resources field', (done) => {
+      const requestBody = { resources: 1 };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('An existing Team cannot update their mindset field', (done) => {
+      const requestBody = { mindset: 1 };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('An existing Team cannot update their mature field', (done) => {
+      const requestBody = { mature: true };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
+
+    it('An existing Team cannot update their link_code field', (done) => {
+      const requestBody = { link_code: 'ASDFGHJ' };
+      sendPatchTeamRequest(requestBody, (err, res) => {
+        res.statusCode.should.equal(400);
+        assert.deepEqual(res.body.request, requestBody);
+        should.exist(err);
+        done();
+      });
+    });
   });
 });
