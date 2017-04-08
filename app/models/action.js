cbc7543c635cb2bd15705fa0e982f1341ace6c7f
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-07 20:00:57 -0400

    Coercion for boolean attributes

--- app/models/action.js
+++ app/models/action.js
@@ -1,7 +1,20 @@
 const bookshelf = require('../lib/bookshelf');
+const has = require('has');
 
 const Action = bookshelf.model('Action', {
   tableName: 'action',
+  parse: (attributes) => {
+    if (has(attributes, 'repeatable')) {
+      if (attributes.repeatable === 1) attributes.repeatable = true;
+      else attributes.repeatable = false;
+    }
+    if (has(attributes, 'requires_mature')) {
+      if (attributes.requires_mature === 1) attributes.requires_mature = true;
+      else attributes.requires_mature = false;
+    }
+
+    return attributes;
+  },
 });
 
 module.exports = Action;
