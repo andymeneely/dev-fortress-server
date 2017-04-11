790489874f84c5106434bd887d62cb8a8efad178
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-11 11:47:53 -0400

    API docs for Game GET routes.

--- app/routes/game.js
+++ app/routes/game.js
@@ -4,7 +4,36 @@ const express = require('express');
 
 const router = express.Router();
 
-// GET all Games
+/**
+ * @api {get} /game Get All Games
+ * @apiName GetGames
+ * @apiGroup Game
+ *
+ * @apiDescription
+ * Retrieve all Games. Requires authentication.
+ *
+ * @apiHeader  {String}     Authorization     Bearer [JWT_TOKEN]
+ *
+ * @apiParam   {String}     [withRelated]     Specify which relations
+ *                                              the Games should be returned with.
+ *                                              Valid options include: 'storyteller' and 'teams'.
+ *                                              One relation per 'withRelated' query param.
+ *
+ * @apiParam   {Integer}    [storyteller_id]  Filter Games matching the storyteller_id.
+ *
+ * @apiSuccess {Game[]}     games                 List of Games.
+ * @apiSuccess {Integer}    games.id              Primary key of the Game.
+ * @apiSuccess {String}     games.name            Custom name of the Game.
+ * @apiSuccess {String}     games.created_at      ISO String timestamp of when the Game was created.
+ * @apiSuccess {Integer}    games.current_round   The current round of the Game.
+ * @apiSuccess {Integer}    games.max_round       The round at which the Game will end.
+ * @apiSuccess {Integer}    games.round_phase     The current round phase the Game is in.
+ *                                                  Dictates what actions can take place.
+ * @apiSuccess {Integer}    games.storyteller_id  The foreign key reference to the
+ *                                                  Professor User running the Game session.
+ * @apiSuccess {User}       [games.storyteller]   The User object representing the Storyteller.
+ *                                                  Only returned when 'withRelated' == 'storyteller'.
+ */
 router.get(
   '/',
   authMiddleware.validateAuthentication,
@@ -19,7 +48,33 @@ router.post(
   gameController.createGame
 );
 
-// GET Game by id
+/**
+ * @api {get} /game Get Game by ID
+ * @apiName GetGame
+ * @apiGroup Game
+ *
+ * @apiDescription
+ * Retrieve Game by ID. Requires authentication.
+ *
+ * @apiHeader  {String}     Authorization     Bearer [JWT_TOKEN]
+ *
+ * @apiParam   {String}     [withRelated]     Specify which relations
+ *                                              the Games should be returned with.
+ *                                              Valid options include: 'storyteller' and 'teams'.
+ *                                              One relation per 'withRelated' query param.
+ *
+ * @apiSuccess {Integer}    id                Primary key.
+ * @apiSuccess {String}     name              Custom name of the Game.
+ * @apiSuccess {String}     created_at        ISO String timestamp of when the Game was created.
+ * @apiSuccess {Integer}    current_round     The current round of the Game
+ * @apiSuccess {Integer}    max_round         The round at which the Game will end.
+ * @apiSuccess {Integer}    round_phase       The current round phase the Game is in.
+ *                                              Dictates what actions can take place.
+ * @apiSuccess {Integer}    storyteller_id    The foreign key reference to the
+ *                                              Professor user running the Game session.
+ * @apiSuccess {User}       [storyteller]     The User object representing the Storyteller.
+ *                                              Only returned when 'withRelated' == 'storyteller'.
+ */
 router.get(
   '/:id',
   authMiddleware.validateAuthentication,
