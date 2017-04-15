5d7cc5fd0c221f144745e38b2b16253e925e5ddb
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-15 14:58:34 -0400

    Seed additional events

--- test/data/seeds/01_seed_events.js
+++ test/data/seeds/01_seed_events.js
@@ -1,18 +1,37 @@
 exports.seed = knex =>
-  knex.transaction((trx) => {
-    knex('event').transacting(trx).insert({
-      id: 1,
-      name: 'Test',
-      description: 'This is a test',
-      default_damage: 100,
-      disabled: 1,
-    })
-    .then(trx.commit)
-    .catch(
-      (err) => {
-        trx.rollback();
-        console.error(err);
-        process.exit(1);
-      }
-    );
-  });
+  Promise.all([
+    knex.transaction((trx) => {
+      knex('event').transacting(trx).insert({
+        id: 1,
+        name: 'Test',
+        description: 'This is a test',
+        default_damage: 100,
+        disabled: 0,
+      })
+      .then(trx.commit)
+      .catch(
+        (err) => {
+          trx.rollback();
+          console.error(err);
+          process.exit(1);
+        }
+      );
+    }),
+    knex.transaction((trx) => {
+      knex('event').transacting(trx).insert({
+        id: 2,
+        name: 'Aliens Attack',
+        description: 'Aliens attack your server room. All applications are affected.',
+        default_damage: 50,
+        disabled: 0,
+      })
+      .then(trx.commit)
+      .catch(
+        (err) => {
+          trx.rollback();
+          console.error(err);
+          process.exit(1);
+        }
+      );
+    }),
+  ]);
