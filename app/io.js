ff81c6c68381b279d7af5bb3c724ef82b2998d43
Joseph DeSimpliciis <jcdesimp@gmail.com>
2017-04-16 16:24:45 -0400

    More socket experimentation.

--- app/io.js
+++ app/io.js
@@ -7,8 +7,18 @@ const ioOptions = {
 
 const io = new Server(ioOptions);
 
-io.on('connetion', (socket) => {
-  console.log(socket);
-})
+
+io.use((socket, next) => {
+  console.log(socket.handshake.query.token);
+  next();
+});
+
+io.on('connection', (socket) => {
+  console.log('Connected!');
+  // console.log(util.inspect(socket.request))
+  console.log(socket.id);
+  socket.emit('current_games', []);
+  io.emit('game_start', {id: 1});
+  io.emit('game_end', {id: 1});
 
 module.exports = io;
