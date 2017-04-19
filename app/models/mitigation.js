f4444fa615a38b7b8ed5ed019ffba4097c877771
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-19 12:33:04 -0400

    Ensure data is returned as a json object, regardless of DB environment.

--- app/models/mitigation.js
+++ app/models/mitigation.js
@@ -4,6 +4,15 @@ require('./event');
 
 const Mitigation = bookshelf.model('Mitigation', {
   tableName: 'mitigation',
+  parse: (attributes) => {
+    if (attributes.data) {
+      if (typeof attributes.data === 'string') {
+        const mitigationData = attributes.data;
+        attributes.data = JSON.parse(mitigationData);
+      }
+    }
+    return attributes;
+  },
   event() {
     return this.belongsTo('Event');
   },
