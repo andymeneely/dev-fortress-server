120be98a0f03a93c07348352881b6022c0faabcc
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-23 13:36:35 -0400

    Add emitter helpers.

--- app/socket/emitters/emitters.js
+++ app/socket/emitters/emitters.js
@@ -5,6 +5,10 @@ function emitToSocket(socket, eventName, eventData) {
   socket.emit(eventName, eventData);
 }
 
+function emitToSocketRoom(socket, room, eventName, eventData) {
+  socket.to(room).emit(eventName, eventData);
+}
+
 function emitInfoEventResults(socket, eventResultName, didSucceed, message) {
   const eventData = {
     event: eventResultName,
@@ -14,7 +18,17 @@ function emitInfoEventResults(socket, eventResultName, didSucceed, message) {
   emitToSocket(socket, 'info', eventData);
 }
 
+function emitInfoFromSocketToRoom(socket, eventResultName, didSucceed, message, room) {
+  const eventData = {
+    event: eventResultName,
+    message,
+    didSucceed,
+  };
+  emitToSocketRoom(socket, room, 'info', eventData);
+}
+
 module.exports = {
   emitToSocket,
   emitInfoEventResults,
+  emitInfoFromSocketToRoom,
 };
