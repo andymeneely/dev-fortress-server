a1a5ff2169097624bab37b0c5c05f410e125155d
Shayde Nofziger <shaydenofziger@gmail.com>
2017-04-23 13:36:54 -0400

    Better validation and error handling for team authentication

--- app/socket/middleware/authentication.js
+++ app/socket/middleware/authentication.js
@@ -2,32 +2,29 @@
  * @module socket/middleware/authentication
  */
 const authLib = require('../../lib/authentication');
-const teamController = require('../controllers/team');
 const emitters = require('../emitters/emitters');
 const has = require('has');
 
 /**
- * Validate a given team token. If valid, update team info in redis.
+ * Validate a given team token.
  * @param {Socket} socket the socket io socket
  * @param {String} token  the team's auth token
  */
-function validateTeamToken(socket, token) {
-  let didSucceed;
+function validateTeamToken(socket, token, next) {
   let message;
-  const decoded = authLib.verifyToken(token);
-  if (!decoded || !has(decoded, 'type') || !has(decoded, 'id') || decoded.type !== 'TEAM') {
-    message = 'Could not verify token!';
-    didSucceed = false;
-    console.error(message);
-  } else {
-    message = `Socket ${socket.id} validated successfully!`;
-    didSucceed = true;
-    console.log(message);
-
-    teamController.storeSocketIdTeamId(socket.id, decoded.id);
-    teamController.storeUpdateTeamInfo(decoded.id);
-  }
-  emitters.emitInfoEventResults(socket, 'authenticate_team', didSucceed, message);
+  let didSucceed;
+  authLib.verifyToken(token, (err, decoded) => {
+    if (err || !decoded || !has(decoded, 'type') || !has(decoded, 'id') || decoded.type !== 'TEAM') {
+      message = 'Could not verify token!';
+      didSucceed = false;
+      console.error(message);
+    } else {
+      message = `Team ${decoded.id} has authenticated successfully!`;
+      didSucceed = true;
+    }
+    emitters.emitInfoEventResults(socket, 'authentication', didSucceed, message);
+    if (didSucceed) next();
+  });
 }
 
 module.exports = {
