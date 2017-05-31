f695f750d8eda1c1d87ce06aaafbabfdea9da69f
Shayde Nofziger <shaydenofziger@gmail.com>
2017-05-31 14:57:02 -0400

    Remove unused views setup/config

--- app/index.js
+++ app/index.js
@@ -8,10 +8,6 @@ require('./socket/handlers/handlers');
 
 const app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
