61f1194904ada2e542f589478d384932c951f57f
Shayde Nofziger <shaydenofziger@gmail.com>
2017-06-07 06:35:04 -0500

    Use util methods; check for fields on req body

--- app/controllers/authentication.js
+++ app/controllers/authentication.js
@@ -1,5 +1,7 @@
 const User = require('../models/user');
 const authentication = require('../lib/authentication');
+const utils = require('../lib/utils');
+const has = require('has');
 
 /**
  * Pre-req: User is authenticated and model data is attached to req.user
@@ -11,10 +13,7 @@ function refreshToken(req, res) {
   /* istanbul ignore if */
   if (!(req.user) && !(req.team)) {
     console.error('No user or team data attached to req.body. AttachUser or AttachTeam middleware is required.');
-    res.status(500).json({
-      error: 'Internal Server Error',
-      message: 'The server is configured incorrectly.',
-    });
+    utils.sendError(500, 'The server is configured incorrectly.', {}, res);
   }
   // Set token data specific to the team or user depending on which is attached to the request.
   const tokenId = req.user ? req.user.id : req.team.id;
@@ -25,10 +24,8 @@ function refreshToken(req, res) {
   };
   authentication.signToken(tokenData, (err, theToken) => {
     if (err) {
-      res.status(500).json({
-        error: err,
-        request: req.body,
-      });
+      console.error('An error occured while signing the refresh token.');
+      utils.sendError(500, 'Internal server error', {}, res);
     } else {
       res.status(200).json({
         token: theToken,
@@ -44,6 +41,12 @@ function refreshToken(req, res) {
  * @param  {Function} next - pass to next route handler
  */
 function login(req, res) {
+  if (!has(req.body, 'username')) {
+    return utils.sendError(400, 'Missing required "username" field.', req.body, res);
+  }
+  if (!has(req.body, 'password')) {
+    return utils.sendError(400, 'Missing required "password" field.', req.body, res);
+  }
   const userDataPromise = User.where('username', req.body.username.toLowerCase()).fetch({
     require: true,
     withRelated: ['password'],
@@ -60,13 +63,10 @@ function login(req, res) {
     })
   );
 
-  Promise.all([userDataPromise, checkPasswordPromise])
+  return Promise.all([userDataPromise, checkPasswordPromise])
   .then(([userData, passMatch]) => {
     if (!passMatch) {
-      res.status(400).json({
-        error: 'BadPassword',
-        message: 'Password is incorrect for user',
-      });
+      utils.sendError(400, 'Username or password is incorrect.', req.body, res);
     }
 
     // Attach User data to the request. Required by refreshToken
@@ -77,18 +77,11 @@ function login(req, res) {
   .catch((err) => {
     /* istanbul ignore else */
     if (err.message === 'EmptyResponse') {
-      res.status(400)
-      .json({
-        error: 'NoUser',
-        message: 'Username not found',
-      });
+      utils.sendError(400, 'Username or password is incorrect.', req.body, res);
     } else {
       // Unknown error
       console.error(err);
-      res.status(500)
-        .json({
-          error: 'UnknownError',
-        });
+      utils.sendError(500, 'An unknown error occured.', req.body, res);
     }
   });
 }
